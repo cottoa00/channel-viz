1ba7497fb5f186305c1be8d64c52fadf1cb2ec96
Anthony Cotton <cottoa00@yahoo.com>
2014-02-25 12:51:23 -0500

    Update follows.js
    Fix defaultFeeds

--- follows.js
+++ follows.js
@@ -13,7 +13,7 @@
 	*/
 
 	var defaultKey		= '87ynD20hZioKhBrsFEywmxEVX7nBI2c0794RW2pR6y9c2OpI', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [1725370504!0], // Comma separated array of Xively Feed ID numbers
+		defaultFeeds	= ['1725370504!0'], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'My Home Electricity Monitor', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
