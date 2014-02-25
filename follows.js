8f534ede0249467aee1eeedf5c7ed36c56a994b8
Anthony Cotton <cottoa00@yahoo.com>
2014-02-25 16:57:16 -0500

    Update follows.js
    Switch 90day view to 1hour view

--- follows.js
+++ follows.js
@@ -83,11 +83,11 @@
 					var updated = new Date;
 					updated = updated.parseISO(datastream.at);
 					var diff = null;
+					if(duration == '1hour') diff = 3600000;
 					if(duration == '6hours') diff = 21600000;
-					 if(duration == '1day') diff = 86400000;
-					 if(duration == '1week') diff = 604800000;
-					 if(duration == '1month') diff = 2628000000;
-					 if(duration == '90days') diff = 7884000000;
+					if(duration == '1day') diff = 86400000;
+					if(duration == '1week') diff = 604800000;
+					if(duration == '1month') diff = 2628000000;
 					then.setTime(now.getTime() - diff);
 					if(updated.getTime() > then.getTime()) {
 						if(datastreamIds && datastreamIds != '' && datastreamIds.indexOf(datastream.id) >= 0) {
@@ -324,6 +324,12 @@
 
 					$('#feed-' + data.id + ' .duration-hour').click(function() {
 						$('#loadingData').foundation('reveal', 'open');
+						updateFeeds(data.id, thisFeedDatastreams, '1hour', 5);
+						return false;
+					});
+
+					$('#feed-' + data.id + ' .duration-hour').click(function() {
+						$('#loadingData').foundation('reveal', 'open');
 						updateFeeds(data.id, thisFeedDatastreams, '6hours', 30);
 						return false;
 					});
@@ -343,12 +349,6 @@
 					$('#feed-' + data.id + ' .duration-month').click(function() {
 						$('#loadingData').foundation('reveal', 'open');
 						updateFeeds(data.id, thisFeedDatastreams, '1month', 1800);
-						return false;
-					});
-
-					$('#feed-' + data.id + ' .duration-90').click(function() {
-						$('#loadingData').foundation('reveal', 'open');
-						updateFeeds(data.id, thisFeedDatastreams, '90days', 10800);
 						return false;
 					});
 
