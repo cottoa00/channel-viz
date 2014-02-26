735f32e725bbef99243640b792c021e80eb6e4f0
Anthony Cotton <cottoa00@yahoo.com>
2014-02-26 10:33:01 -0500

    Update follows.js
    Absolute hardcode of location

--- follows.js
+++ follows.js
@@ -457,7 +457,8 @@
 	$('#setFeeds').click(function() {
 		setApiKey($('#apiKeyInput').val());
 		feeds = $('#feedsInput').val().replace(/\s+/g, '').split(',');
-		window.location.href = './index.html#key=87ynD20hZioKhBrsFEywmxEVX7nBI2c0794RW2pR6y9c2OpI&feeds=1725370504!2';
+		window.location = 'http://cottoa00.github.io/channel-viz/index.html#key=87ynD20hZioKhBrsFEywmxEVX7nBI2c0794RW2pR6y9c2OpI&feeds=1725370504!2';
+//		window.location.href = './index.html#key=87ynD20hZioKhBrsFEywmxEVX7nBI2c0794RW2pR6y9c2OpI&feeds=1725370504!2';
 //		window.location = './index.html#key=87ynD20hZioKhBrsFEywmxEVX7nBI2c0794RW2pR6y9c2OpI&feeds=1725370504!2';
 //		window.location = './index.html#key=' + '87ynD20hZioKhBrsFEywmxEVX7nBI2c0794RW2pR6y9c2OpI' + '&feeds=' + '1725370504!2';
 //		window.location = './index.html#key=' + $('#apiKeyInput').val() + '&feeds=' + $('#feedsInput').val();
